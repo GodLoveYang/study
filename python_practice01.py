d2102805591f5281826a97c5b98d8b85bf2b88fa
GodLoveYang <1354412764@qq.com>
2021-01-31 10:41:12 +0800

    作业

--- python_practice01.py
+++ python_practice01.py
@@ -8,3 +8,31 @@ my_hp = hp - enemy_power
 enemy_final_hp = enemy_hp - my_power
 谁的hp先为0，那么谁就输了
 """
+
+def fight():
+    enemy_name = input("请输入角色名称：")
+    enemy_hp = int(input("请输入角色血量："))
+    enemy_power = int(input("请输入角色力量："))
+    my_power = 200
+    hp = 1000
+    my_name = "test"
+    while True:
+        hp = hp - enemy_power
+        enemy_hp = enemy_hp - my_power
+        print("我的血量：{},敌人的血量：{}".format(hp,enemy_hp))
+        if enemy_hp<=0:
+            print("{}输了".format(enemy_name))
+            break
+        elif hp<=0:
+            print("{}输了".format(my_name))
+            break
+
+# def role_messages():
+#     enemy_name = input("请输入角色名称：")
+#     enemy_hp = int(input("请输入角色血量："))
+#     enemy_power = int(input("请输入角色力量："))
+#     return enemy_name,enemy_hp,enemy_power
+
+if __name__ == "__main__":
+    # role_messages()
+    fight()
